6d26c3ad8604d59cfd7030c662175aa89bf8ce82
Ruben Rodriguez II <cha0s@therealcha0s.net>
2013-03-05 14:45:35 -0600

    Remove low level Image data API

--- Graphics/Image.h
+++ Graphics/Image.h
@@ -46,11 +46,6 @@ public:
 	Image();
 
 	/**
-	 * Build an image from memory.
-	 */
-	Image(void *data, unsigned int length);
-
-	/**
 	 * Build an image from a filename.
 	 */
 	Image(const boost::filesystem::path &uri);
@@ -236,7 +231,6 @@ public:
 	virtual ~AbstractFactory<Image>() {}
 
 	virtual Image *create() = 0;
-	virtual Image *create(void *data, unsigned int length) = 0;
 	virtual Image *create(int width, int height) = 0;
 	virtual Image *create(const boost::filesystem::path &uri) = 0;
 
